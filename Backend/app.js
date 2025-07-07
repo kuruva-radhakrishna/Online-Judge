df7c3a56f4cb644228457d91063bed324a86895a
kuruva-radhakrishna <kuruvaradhakrishna77@gmail.com>
2025-07-07 19:33:54 +0530

    Mongostore for sessions

--- Backend/app.js
+++ Backend/app.js
@@ -9,6 +9,7 @@ const port = process.env.PORT;
 const LocalStrategy = require("passport-local").Strategy;
 const passport = require("passport");
 const session = require("express-session");
+const MongoStore = require('connect-mongo');
 
 const User = require("./Models/user.js");
 const UserRoute = require("./Routes/UserRoute.js");
@@ -30,10 +31,9 @@ const allowedOrigins = [
 
 app.use(cors({
   origin: allowedOrigins,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,  
-  allowedHeaders: ['Content-Type', 'Authorization']
+  credentials: true
 }));
+
 
 connection();
 
@@ -47,19 +47,19 @@ passport.deserializeUser(User.deserializeUser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const isProd = process.env.NODE_ENV === 'production';
-
-app.use(session({
-  secret: "yourSecretKey",
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    httpOnly: true, // extra security
-    secure: isProd, // only true in production
-    sameSite: isProd ? "none" : "lax", // fix here
-    maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
-  }
-}));
+app.use(
+    session({
+        secret: "yourSecretKey",
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({ mongoUrl: process.env.MONGOOSE_URL }),
+        cookie: {
+            sameSite: "none",
+            secure: true,
+            maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days, optional
+        },
+    })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
